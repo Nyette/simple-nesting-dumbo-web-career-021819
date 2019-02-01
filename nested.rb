dfcc8a3b695a7fda58e6f07175196814b730c62a
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-02-01 09:45:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -76,7 +76,10 @@ def adding_matz
           :languages => ["C"]
         }
   }
-  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+  programmer_hash[:yukihiro_matsumoto] = {
+    :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+  }
   programmer_hash
 end
 
@@ -97,11 +100,11 @@ def changing_alan
   }
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
   programmer_hash[:alan_kay][:known_for] = "GUI"
+  programmer_hash
 end
 
 def adding_to_dennis
-	programmer_hash = 
- 		{
+	programmer_hash = {
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -114,6 +117,7 @@ def adding_to_dennis
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
-
+  }
+  programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
+  programmer_hash
 end
